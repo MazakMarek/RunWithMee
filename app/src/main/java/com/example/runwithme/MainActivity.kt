8bb37f3d65f0487f234e5ca6feefd80c5ad69705
MazakMarek <149437498+MazakMarek@users.noreply.github.com>
2024-05-03 00:28:37 +0300

    start

--- app/src/main/java/com/example/runwithme/MainActivity.kt
+++ app/src/main/java/com/example/runwithme/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.runwithme.ui.theme.RunWithMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Ahoj  $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RunWithMeTheme {
-        Greeting("Android")
+        Greeting("Katka")
     }
 }
